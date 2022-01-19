e198f3de1553322eb7f029e4bca5e789ff64ec8d
ibtihajAmin <ibtihaj.amin18@gmail.com>
2022-01-19 21:46:05 +0600

    Small Changes are added

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,51 +1,86 @@
-import React, { useState } from 'react';
-import { Grid, Container, Typography, Alert } from '@mui/material';
-import Booking from '../Booking/Booking';
+import React, { useState } from "react";
+import { Grid, Container, Typography, Alert } from "@mui/material";
+import Booking from "../Booking/Booking";
 
 const bookings = [
-    {
-        id: 1,
-        name: 'ENT',
-        time: '6.00 PM- 7.00 PM',
-        space: 20
-    },
-    {
-        id: 2,
-        name: 'Orthopedic',
-        time: '7.00 PM- 8.00 PM',
-        space: 20
-    },
-    {
-        id: 3,
-        name: 'Child Specialist',
-        time: '6.00 PM- 7.00 PM',
-        space: 25
-    },
-    {
-        id: 4,
-        name: 'ENT',
-        time: '6.00 PM- 7.00 PM',
-        space: 20
-    },
-]
+  {
+    id: 1,
+    name: "ENT",
+    time: "6.00 PM- 7.00 PM",
+    space: 20,
+  },
+  {
+    id: 2,
+    name: "Orthopedic",
+    time: "7.00 PM- 8.00 PM",
+    space: 20,
+  },
+  {
+    id: 3,
+    name: "Neurology",
+    time: "6.00 PM- 7.00 PM",
+    space: 25,
+  },
+  {
+    id: 4,
+    name: "Medicine",
+    time: "7.00 PM- 8.00 PM",
+    space: 20,
+  },
+  {
+    id: 5,
+    name: "Oncology",
+    time: "5.00 PM- 6.00 PM",
+    space: 20,
+  },
+  {
+    id: 6,
+    name: "Paediatric",
+    time: "4.00 PM- 5.00 PM",
+    space: 20,
+  },
+  {
+    id: 7,
+    name: "Pulmonology",
+    time: "7.00 PM- 8.00 PM",
+    space: 20,
+  },
+  {
+    id: 8,
+    name: "Urology",
+    time: "7.00 PM- 8.00 PM",
+    space: 20,
+  },
+  {
+    id: 9,
+    name: "Cardiology",
+    time: "6.00 PM- 7.00 PM",
+    space: 20,
+  },
+];
 
 const AvailableAppointment = ({ date }) => {
-    const [bookingSuccess, setBookingSuccess] = useState(false)
-    return (
-        <Container>
-            <Typography variant="h4" sx={{ color: 'info.main', my: 3 }}>Available Appoinntment {date.toDateString()} </Typography>
-            {bookingSuccess && <Alert severity="success">Appointment Successfully.</Alert>}
-            <Grid container spacing={2}>
-                {
-                    bookings.map(booking => <Booking
-                        key={booking.id}
-                        booking={booking}
-                        date={date}
-                        setBookingSuccess={setBookingSuccess}></Booking>)
-                }
-            </Grid>
-        </Container>
-    );
+  const [bookingSuccess, setBookingSuccess] = useState(false);
+  return (
+    <Container>
+      <Typography variant="h4" sx={{ color: "info.main", my: 3 }}>
+        Available Appoinntment {date.toDateString()}{" "}
+      </Typography>
+      {bookingSuccess && (
+        <Alert severity="success">Appointment Successfully.</Alert>
+      )}
+      <Grid container spacing={2}>
+        {bookings.map((booking) => (
+          <Booking
+            key={booking.id}
+            booking={booking}
+            date={date}
+            setBookingSuccess={setBookingSuccess}
+          ></Booking>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default AvailableAppointment;
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import top_banner from '../../../images/top_banner.jpg';
-import { Button, Typography, Container } from '@mui/material';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import top_banner from "../../../images/top_banner.jpg";
+import { Button, Typography, Container } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-    return (
-        <Container sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item style={{ textAlign: 'left' }} sx={{ mt: 5, mb: 5 }} xs={12} md={5}>
-                    <Typography variant="h3">
-                        Make Your Appointment <br />
-                        By using This App.
-                    </Typography>
-                    <Typography variant="h5" sx={{ fontSize: 14, fontWeight: 300, color: 'gray', my: 2 }}>
-                        Lorem Ipsum Dolor Simit.Lorem Ipsum Dolor Simit.Lorem Ipsum Dolor Simit.Lorem Ipsum Dolor Simit.
-                    </Typography>
-                    <Button variant='contained' sx={{ my: 2 }}>Get Appointment</Button>
-                </Grid>
-                <Grid item xs={12} md={7} sx={{ mt: 5, mb: 5 }}>
-                    <img style={{ width: '350px' }} src={top_banner} alt="Top" />
-                </Grid>
-
-
-            </Grid>
-        </Container>
-    );
+  return (
+    <Container sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          style={{ textAlign: "left" }}
+          sx={{ mt: 5, mb: 5 }}
+          xs={12}
+          md={5}
+        >
+          <Typography variant="h3">
+            Make Your Appointment <br />
+            By using This App.
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{ fontSize: 14, fontWeight: 300, color: "gray", my: 2 }}
+          >
+            Take Your Appointment and Be safe with happiness.
+          </Typography>
+          <Link to="/appointment">
+            <Button variant="contained" sx={{ my: 2 }}>
+              Get Appointment
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={7} sx={{ mt: 5, mb: 5 }}>
+          <img style={{ width: "350px" }} src={top_banner} alt="Top" />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Banner;
